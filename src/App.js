c2e0c74318d7d31b5415e733de181d8e9a90b9d2
iamsh2580 <iamsh2580@gmail.com>
2023-02-01 21:48:30 +0900

    class-random

--- src/App.js
+++ src/App.js
@@ -5,23 +5,34 @@ function App() {
   return (
     <div className="container">
       <h1>Hello,World!</h1>
-      <FuncCump></FuncCump>
-      <ClassCump></ClassCump>
+      <FuncCump initNumber={2}></FuncCump>
+      <ClassCump initNumber={2}></ClassCump>
     </div>
   );
 }
-function FuncCump(){
+function FuncCump(props){
   return (
     <div className='container'>
       <h2>function style component</h2>
+      <p>Number: {props.initNumber}</p>
     </div>
   );
 }
 class ClassCump extends React.Component{
+  state={
+    number:this.props.initNumber
+  }
   render(){
     return (
       <div className='container'>
         <h2>class style component</h2>
+        <p>Number: {this.state.number}</p>
+        <input type="button" value="random" onClick={
+          function(){
+            this.setState({number:Math.random()})
+          }.bind(this)
+          }>
+        </input>
       </div>
     );
   }

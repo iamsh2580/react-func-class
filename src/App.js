c4435b1130bd0d3743ed76e00d2cfcb59a4e2ec2
iamsh2580 <iamsh2580@gmail.com>
2023-02-02 11:03:10 +0900

    함수에서 state사용

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React ,{useState}from 'react';
 import './App.css';
 
 function App() {
@@ -11,17 +11,37 @@ function App() {
   );
 }
 function FuncCump(props){
+    var numberState=useState(props.initNumber);
+    var number =numberState[0]; /**상태값 */
+    var setNumber=numberState[1]; /**상태를 바꿀 수 있는 함수 */
+
+    // var dateState=useState((new Date()).toString());
+    // var _date =dateState[0]; /**상태값 */
+    // var setDate=dateState[1]; /**상태를 바꿀 수 있는 함수 */
+    //위 3줄의 코드를 한 줄로 줄이기 (아래)
+    var [_date,setDate] = useState((new Date().toString()));
+    
   return (
     <div className='container'>
       <h2>function style component</h2>
-      <p>Number: {props.initNumber}</p>
+      <p>Number: {number}</p>
+      <p>DateL{_date}</p>
+    <input type="button" value="random" onClick={
+      function(){
+        setNumber(Math.random());
+        setDate((new Date()).toString());
+      }
+      }>
+    </input>
     </div>
   );
 }
 class ClassCump extends React.Component{
   state={
-    number:this.props.initNumber
+    number:this.props.initNumber,
+    date:(new Date()).toString()
   }
+
   render(){
     return (
       <div className='container'>
@@ -30,6 +50,13 @@ class ClassCump extends React.Component{
         <input type="button" value="random" onClick={
           function(){
             this.setState({number:Math.random()})
+          }.bind(this)
+          }>
+        </input>
+        <p>Date :{this.state.date}</p>
+        <input type="button" value="random" onClick={
+          function(){
+            this.setState({date:(new Date().toString())})
           }.bind(this)
           }>
         </input>
